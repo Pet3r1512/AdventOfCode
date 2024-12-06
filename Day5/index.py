53bfc5e88cee445f415f407ef6c1e506a05d05a0
Peter Pham <pttp15122002@gmail.com>
2024-12-06 08:21:16 +0700

    day 5 - part 2

--- Day5/index.py
+++ Day5/index.py
@@ -23,13 +23,45 @@ def is_valid_order(order, rule_dict):
     return True
 
 total_sum = 0
+invalid_orders = []
+
+print(total_sum)
 
 for order in orders:
     if is_valid_order(order, dictionary):
-        print(f"Valid order: {order}")
         middle_index = len(order) // 2
         middle_element = order[middle_index]
-        print(f"Middle element: {middle_element}")
         total_sum += middle_element  
+    else:
+        invalid_orders.append(order)
 
-print(f"Total sum: {total_sum}")
+def fix_invalid_order(invalid_order):
+    for i in range(0, len(invalid_order)):
+        new_dict = {}
+        sliced_order = invalid_order[i:]
+        
+        for key, values in dictionary.items():
+            if key in sliced_order:
+                valid_values = [value for value in values if value in sliced_order]
+                if valid_values: 
+                    new_dict[key] = valid_values
+        
+        values_set = set()
+        for values in new_dict.values():
+            values_set.update(values)
+        missing_element = int((set(sliced_order) - values_set).pop())
+        missing_element_index = invalid_order.index(missing_element)
+        invalid_order[i], invalid_order[missing_element_index] = invalid_order[missing_element_index], invalid_order[i]
+
+
+
+    return invalid_order
+
+invalid_sum = 0
+for line in invalid_orders:
+    valid = fix_invalid_order(line)
+    middle_index = len(valid) // 2
+    middle_element = valid[middle_index]
+    invalid_sum += middle_element  
+
+print(invalid_sum)
